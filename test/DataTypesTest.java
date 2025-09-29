89ac11abb7678ba4c392a9665ab55b09a7c08425
Rahul Sahay <rahul.sahay@mail.utoronto.ca>
2025-09-29 13:27:31 -0400

    Completed TASK 1 TODO, checked off completed items so far, and removed word TODO

--- test/DataTypesTest.java
+++ test/DataTypesTest.java
@@ -1,25 +1,25 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class DataTypesTest {
 
-    @Test
     /**
-     * Test that DataTypes.sum returns the correct value for
-     * the sum from 1 to 1 million.
+     * Test that DataTypes.sum returns the correct value
+     * for the sum from 1 to 1 million.
      */
+    @Test
     public void largeSumTest() {
-
-        // You put an L at the end to indicate it is a long.
-        // try removing the L and you'll see that you get an error
-        // saying "Integer number too large".
-        // Note: You can use _ to help make it easier to
-        //       read large numbers, as is done below.
+        // Expected sum from 1 to 1,000,000
         long x = 500_000_500_000L;
+
+        // Build the array of numbers 1..1,000,000
         int[] numbers = new int[1_000_000];
         for (int i = 1; i <= 1_000_000; i++) {
-            numbers[i-1] = i;
+            numbers[i - 1] = i;
         }
-        assertEquals(x, DataTypes.sum(numbers), "sum from 1 to 1 million should be " + x);
+
+        // Check result
+        assertEquals("sum from 1 to 1 million should be " + x,
+                x, DataTypes.sum(numbers));
     }
 }
